10eeba35c5065828088bd09dfe569fa456f28af6
Sachin-Lamay <sachinlamay402@gmail.com>
2024-10-28 11:53:00 +0530

    Add files via upload

---
+++ Q5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int myNum=5;
+    if(myNum%2==0){
+        printf("It is an even number");
+    }else{
+        printf("It is an odd number");
+    }
+    return 0;
+    }
