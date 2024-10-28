e65c12f9e5c74ec69f7314af13ed62e1bb027a6e
Sachin-Lamay <sachinlamay402@gmail.com>
2024-10-28 11:49:58 +0530

    Add files via upload

---
+++ Q3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int myAge=20;
+    int votingAge=18;
+    if(myAge>=votingAge){
+        printf("Old enough to vote");
+    }else{
+        printf("Not old enoiugh to vote");
+    }
+    return 0;
+    }
