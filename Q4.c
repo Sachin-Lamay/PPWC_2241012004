ccd4a9cd1f4fa4be165ddfd047ac6706f37da41d
Sachin-Lamay <sachinlamay402@gmail.com>
2024-10-28 11:50:17 +0530

    Add files via upload

---
+++ Q4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int myNum=7;
+    if(myNum>0){
+        printf("it is positive number");
+    }else{
+        printf("it is negative number");
+    }
+    return 0;
+    }
+
